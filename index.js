9da34bcd7872a5f7d2f96468159bf5322acb1f78
Juan David Osorio Ortiz <jdoo1115@gmail.com>
2024-04-20 19:32:34 -0500

    .

--- index.js
+++ index.js
@@ -9,16 +9,16 @@ dbConnection()
 
 app.use(cors())
 
-app.use(express.static('public'));
-
 app.use(express.json())
 
 app.use('/api/v1/auth', require('./routes/auth.js'))
 app.use('/api/v1/product', require('./routes/product.js'))
 app.use('/api/v1/cart', require('./routes/cart.js'))
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('/', (req, res) => {
+  res.send(`
+    <h1>PT-24008 API</h1>
+  `);
 });
 
 app.listen(process.env.PORT || 3000, () => {
